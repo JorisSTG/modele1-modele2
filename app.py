42eea2eed948a806c00893573889bcd6067e8854
JorisSTG <joris.stg@gmail.com>
2026-01-20 09:42:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -860,8 +860,8 @@ if uploaded_model1 and uploaded_model2:
     # ============================
     st.subheader("DJC (chauffage) et DJF (froid) journaliers")
 
-    T_base_chauffage = float(st.text_input("Base DJC (°C) — chauffage", "19"))
-    T_base_froid = float(st.text_input("Base DJF (°C) — refroidissement", "23"))
+    T_base_chauffage = float(st.text_input("Base DJC (°C) — chauffage", "18"))
+    T_base_froid = float(st.text_input("Base DJF (°C) — refroidissement", "26"))
 
     results_djc = []
     results_djf = []
